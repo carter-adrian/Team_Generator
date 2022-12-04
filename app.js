47fc3fb0a04c8807737d3448555e55a31ee0967b
unknown <carter.adrian@gmail.com>
2022-12-04 12:48:57 -0500

    html added and error correction on line 50

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ function addMember() {
         },
         {
             type: "list",
-            message: "Would you like to add more team members?"
+            message: "Would you like to add more team members?",
             choices: [
                 "yes",
                 "no"
@@ -74,5 +74,25 @@ function addMember() {
 }
 
 function startHTML() {
-    const html = ``
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title>Team Profile</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-dark bg-dark mb-5">
+            <span class="navbar-brand mb-0 h1 w-100 text-center">Team Profile</span>
+        </nav>
+        <div class="container">
+            <div class="row">`;
+    fs.writeFile("./output/employee.html", html, function(err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+    console.log("start");
 }
